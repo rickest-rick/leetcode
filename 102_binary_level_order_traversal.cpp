7439d9ab55fc26f537e9cbe49b79e1021a831f75
Joschi <j.st@wwu.de>
2018-11-07 22:32:00 +0100

    queue solution for 102 bin level order traversal

---
+++ 102_binary_level_order_traversal.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if (!root) return vector<vector<int>>();
+        vector<vector<int>> level_order;
+        int level_size = 1; // number of elements of the current level
+        int new_level_size; // number of elements of the next level
+        queue<TreeNode*> level_queue;
+        level_queue.emplace(root);
+        // as long as nodes are left
+        while (!level_queue.empty()) {
+            new_level_size = 0;
+            vector<int> level(level_size);
+            // for the size of elements of the next level
+            for (int i = 0; i < level_size; i++) {
+                TreeNode* current = level_queue.front();
+                level_queue.pop();
+                // append them to the next level
+                level[i] = current->val;
+                // append every non null child to the level_queue and count
+                if (current->left) {
+                    level_queue.emplace(current->left);
+                    new_level_size++;
+                }
+                if (current->right) {
+                    level_queue.emplace(current->right);
+                    new_level_size++;
+                }
+            }
+            // append level to output
+            level_order.emplace_back(level);
+            level_size = new_level_size;
+        }
+        return level_order;
+    }
+};
